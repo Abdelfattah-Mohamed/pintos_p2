675531dc07b9ca1d3014633cc658daaf82b2792d
Abdelfattah <abdelftahmohamed30@gmail.com>
2019-12-14 09:54:57 +0200

    all tests pass

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -8,15 +8,15 @@
 #include "process.h"
 
 static void syscall_handler (struct intr_frame *);
-void *check_valid(const void *checker);
+void* validation(const void*);
 struct proc_file* get_file(struct list* files, int fd);
+void check_buffer (void *buff_to_check, unsigned size);
+void syscall_check_user_string(const char *ustr);
 
-extern bool running;
-
-struct proc_file {	
-    struct file* file;	
-    int fd;	
-    struct list_elem elem;	
+struct proc_file {
+	struct file* file;
+	int fd;
+	struct list_elem elem;
 };
 
 void
@@ -28,96 +28,120 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-	check_valid(f->esp);
+  //int * p = f->esp;
+
+	validation((int *) f->esp);
+	validation(f->esp + 3);
 
 
+
+  //int system_call = * p;
 	switch (*(int *) f->esp)
 	{
 		case SYS_HALT: {
 			shutdown_power_off();
-			break;
+			break;	
 		}
+		
 		case SYS_EXIT: {
-			check_valid((int *) f->esp + 1);
+			validation((int *) f->esp + 1);
+			validation(f->esp + 7);
 			exit(*((int *) f->esp + 1));
+			break;	
+		}
+		
+		case SYS_EXEC: {
+			validation((int *) f->esp + 1);
+			validation(f->esp + 7);
+			validation(*((int *) f->esp + 1));
+			syscall_check_user_string(*((int *) f->esp + 1));
+			f->eax = excu(*((int *) f->esp + 1));
 			break;
 		}
-		case SYS_EXEC:{
-		check_valid((int *) f->esp + 1);
-		check_valid(*((int *) f->esp + 1));
-		f->eax = exec(*((int *) f->esp + 1));
-		break;
-}
-		case SYS_WAIT:{
-		check_valid((int *) f->esp + 1);
-		f->eax = process_wait(*((int *) f->esp + 1));
-		break;
-}
-		case SYS_CREATE:{
-		acquire_filesys_lock();    // Make sure only one process hold the file system at a time.	
-        check_valid((int *) f->esp + 2);    // check second argument's address.	
-        check_valid(*((int *) f->esp + 1));    // check first argument's content.	
-        f->eax = filesys_create(*((int *) f->esp + 1),*((int *) f->esp + 2));	
-        release_filesys_lock();    // Release our lock.
-		break;
-}
-		case SYS_REMOVE: {
-		acquire_filesys_lock();    // Make sure only one process hold the file system at a time.	
-        check_valid((int *) f->esp + 1);    // check first argument's address.	
-        check_valid(*((int *) f->esp + 1));    // check first argument's content.	
-         	
-        if(!filesys_remove(*((int *) f->esp + 1))) f->eax = false;	
-        else f->eax = true;	
-        release_filesys_lock();    // Release our lock.
-		break;
-}
-		case SYS_OPEN: {
-		acquire_filesys_lock();    // Make sure only one process hold the file system at a time.	
-        int fd = -1;    // initialize file descriptor with invalid value.	
-        check_valid((int *) f->esp + 1);    // check first argument's address.	
-        check_valid(*((int *) f->esp + 1));    // check first argument's content.	
-        char *file = (char *) (*((int *) f->esp + 1));  // keep file name (argument[0]).	
-        struct file *of = filesys_open(file);   // open file.	
-        if (of) {	
-            // Create a struct to hold the file/fd.	
-        	struct proc_file *pf = malloc(sizeof(struct proc_file));	
-            pf->file = f;	
-            pf->fd = thread_current()->fd;  // generate fd for current file.	
-            thread_current()->fd++;   // Increment the fd for future files.	
-            // add file to file descriptors list in thread.	
-            list_push_back(&thread_current()->file_descriptors, &pf->elem);	
-            fd = pf->fd;	
-        }	
-        f->eax = fd;    // update f->eax with fd.	
-        release_filesys_lock();    // Release our lock.	
-        break;
-}
-		case SYS_FILESIZE: {
-		acquire_filesys_lock();    // Make sure only one process hold the file system at a time.
-		check_valid((int *) f->esp + 1);    // check first argument's address.	
-		struct proc_file *pf = get_file(&thread_current()->file_descriptors, *((int *) f->esp + 1));   // get file.	
-        struct file *ff = pf->file;	
-        if (ff) f->eax = file_length(ff);  // get file length.	
-        else f->eax = -1;   // if not found.	
-        release_filesys_lock();    // Release our lock.
-		break;
+
+		case SYS_WAIT:	{
+			validation((int *) f->esp + 1);
+			validation(f->esp + 7);
+			f->eax = process_wait(*((int *) f->esp + 1));
+			break;
 		}
-		case SYS_READ: {
-		check_valid((int *) f->esp + 3);
-		check_valid(*((int *) f->esp + 2));
+		
+		case SYS_CREATE: {
+			validation((int *) f->esp + 2);    // check second argument's address.
+			validation(f->esp + 11);
+	        validation(*((int *) f->esp + 1));    // check first argument's content.
+	        syscall_check_user_string(*((int *) f->esp + 1));
+	        check_buffer((void *) *((int *) f->esp + 1), *((int *) f->esp + 2));	
+	        acquire_filesys_lock();    // Make sure only one process hold the file system at a time.
+	        f->eax = filesys_create(*((int *) f->esp + 1),*((int *) f->esp + 2));	
+	        release_filesys_lock();    // Release our lock.
+			break;
+		}
+		case SYS_REMOVE: {
+			validation((int *) f->esp + 1);    // check first argument's address.	
+			validation(f->esp + 7);
+	        validation(*((int *) f->esp + 1));    // check first argument's content.	
+	        syscall_check_user_string(*((int *) f->esp + 1));
+	        acquire_filesys_lock();    // Make sure only one process hold the file system at a time.	
+	        if(!filesys_remove(*((int *) f->esp + 1))) f->eax = false;	
+	        else f->eax = true;	
+	        release_filesys_lock();    // Release our lock.
+			break;
+		}
+
+		case SYS_OPEN: {
+			int fd = -1;    // initialize file descriptor with invalid value.	
+		    validation((int *) f->esp + 1);    // check first argument's address.
+		    validation(f->esp + 7);	
+		    validation(*((int *) f->esp + 1));    // check first argument's content.	
+		    syscall_check_user_string(*((int *) f->esp + 1));
+		    acquire_filesys_lock();    // Make sure only one process hold the file system at a time.	
+		    //char *file = (char *) ;  // keep file name (argument[0]).	
+		    struct file *of = filesys_open((*((int *) f->esp + 1)));   // open file.
+		    release_filesys_lock();    // Release our lock.	
+		    if (of != NULL) {	
+		        // Create a struct to hold the file/fd.	
+		    	struct proc_file *pf = malloc(sizeof(struct proc_file));	
+		        pf->file = of;	
+		        pf->fd = thread_current()->fd;  // generate fd for current file.	
+		        thread_current()->fd++;   // Increment the fd for future files.	
+		        // add file to file descriptors list in thread.	
+		        list_push_back(&thread_current()->file_descriptors, &pf->elem);	
+		        fd = pf->fd;	
+		    }	
+		    f->eax = fd;    // update f->eax with fd.	
+		    break;
+		}
+		case SYS_FILESIZE: {
+			validation((int *) f->esp + 1);    // check first argument's address.
+			validation(f->esp + 7);
+			acquire_filesys_lock();    // Make sure only one process hold the file system at a time.
+			struct proc_file* pf =  malloc(sizeof(struct proc_file));
+			pf = get_file(&thread_current()->file_descriptors, *((int *) f->esp + 1));   // get file.	
+	        struct file *ff = pf->file;	
+	        if (ff) f->eax = file_length(ff);  // get file length.	
+	        else f->eax = -1;   // if not found.	
+	        release_filesys_lock();    // Release our lock.
+			break;
+		}
+		case SYS_READ:
+		validation((int *) f->esp + 3);
+		validation(f->esp + 15);
+		validation(*((int *) f->esp + 2));
+		check_buffer((void *) *((int *) f->esp + 2), *((int *) f->esp + 3));
 		if(*((int *) f->esp + 1) == 0)
 		{
 			int i;
 			uint8_t* buffer = *((int *) f->esp + 2);
-			for(i = 0; i < *((int *) f->esp + 3); i++)
+			for(i=0;i<*((int *) f->esp + 3);i++)
 				buffer[i] = input_getc();
 			f->eax = *((int *) f->esp + 3);
 		}
 		else
 		{
 			struct proc_file* fptr = get_file(&thread_current()->file_descriptors, *((int *) f->esp + 1));
-			if(fptr == NULL)
-				f->eax = -1;
+			if(fptr==NULL)
+				f->eax=-1;
 			else
 			{
 				acquire_filesys_lock();
@@ -126,20 +150,22 @@ syscall_handler (struct intr_frame *f UNUSED)
 			}
 		}
 		break;
-}
-		case SYS_WRITE: {
-		check_valid((int *) f->esp + 3);
-		check_valid(*((int *) f->esp + 2));
+
+		case SYS_WRITE:
+		validation((int *) f->esp + 3);
+		validation(f->esp + 15);
+		validation(*((int *) f->esp + 2));
+		check_buffer((void *) *((int *) f->esp + 2), *((int *) f->esp + 3));
 		if(*((int *) f->esp + 1) == 1)
 		{
-			putbuf(*((int *) f->esp + 2),*((int *) f->esp + 3));
+			putbuf(*((int *) f->esp + 2), *((int *) f->esp + 3));
 			f->eax = *((int *) f->esp + 3);
 		}
 		else
 		{
 			struct proc_file* fptr = get_file(&thread_current()->file_descriptors, *((int *) f->esp + 1));
-			if(fptr == NULL)
-				f->eax = -1;
+			if(fptr==NULL)
+				f->eax=-1;
 			else
 			{
 				acquire_filesys_lock();
@@ -148,62 +174,64 @@ syscall_handler (struct intr_frame *f UNUSED)
 			}
 		}
 		break;
-}
+
 		case SYS_SEEK: {
-		acquire_filesys_lock();    // Make sure only one process hold the file system at a time.	
-        check_valid((int *) f->esp + 1);    // check first argument's address.	
-        check_valid((int *) f->esp + 2);    // check second argument's address.	
-        struct proc_file* pf = get_file(&thread_current()->file_descriptors, *((int *) f->esp + 1));	
-        struct file *ff = pf->file;   // get file.	
-        if (ff) file_seek(ff, *((int *) f->esp + 2));  // seek file.	
-        release_filesys_lock();    // Release our lock.
-		break;
+			validation((int *) f->esp + 1);    // check first argument's address.	
+			validation(f->esp + 7);
+	        validation((int *) f->esp + 2);    // check second argument's address.	
+	        validation(f->esp + 11);
+	        acquire_filesys_lock();    // Make sure only one process hold the file system at a time.	
+	        struct proc_file* pf = get_file(&thread_current()->file_descriptors, *((int *) f->esp + 1));	
+	        struct file *ff = pf->file;   // get file.	
+	        if (ff) file_seek(ff, *((int *) f->esp + 2));  // seek file.	
+	        release_filesys_lock();    // Release our lock.
+			break;
 		}
 		case SYS_TELL: {
-		acquire_filesys_lock();    // Make sure only one process hold the file system at a time.	
-        check_valid((int *) f->esp + 1);    // check first argument's address.	
-        struct proc_file* pf = get_file(&thread_current()->file_descriptors, *((int *) f->esp + 1));   // get file.	
-        struct file *ff = pf->file;   // get file.	
-        if (ff) f->eax = file_tell(f);  // tell file.	
-        else f->eax = -1;
-        release_filesys_lock();   // Release our lock.
-		break;
+			validation((int *) f->esp + 1);    // check first argument's address.	
+			validation(f->esp + 7);
+	        acquire_filesys_lock();    // Make sure only one process hold the file system at a time.
+	        struct proc_file* pf = get_file(&thread_current()->file_descriptors, *((int *) f->esp + 1));   // get file.	
+	        struct file *ff = pf->file;   // get file.	
+	        if (ff) f->eax = file_tell(f);  // tell file.	
+	        else f->eax = -1;
+	        release_filesys_lock();   // Release our lock.
+			break;
 		}
 		case SYS_CLOSE: {
-		acquire_filesys_lock();    // Make sure only one process hold the file system at a time.	
-        check_valid((int *) f->esp + 1);    // check first argument's address.	
-        close_file(&thread_current()->file_descriptors, *((int *) f->esp + 1));  // close file.	
-        release_filesys_lock();    // Release our lock.
-		break;
-}
+			validation((int *) f->esp + 1);
+			validation(f->esp + 7);
+			acquire_filesys_lock();
+			close_file(&thread_current()->file_descriptors, *((int *) f->esp + 1));
+			release_filesys_lock();
+			break;
+		}
 
-		default:
-		printf("Default %d\n",*((int *) f->esp + 1));
+		default: {
+			printf("Default %d\n", *(int *) f->esp);	
+		}
 	}
 }
 
-int exec(char *file_name)
+int excu(char *file_name)
 {
 	acquire_filesys_lock();
 	char * fn_cp = malloc (strlen(file_name)+1);
-	  strlcpy(fn_cp, file_name, strlen(file_name)+1);
+	strlcpy(fn_cp, file_name, strlen(file_name)+1);
 	  
-	  char * save_ptr;
-	  fn_cp = strtok_r(fn_cp," ",&save_ptr);
+	char * save_ptr;
+	fn_cp = strtok_r(fn_cp," ",&save_ptr);
 
-	 struct file* f = filesys_open (fn_cp);
+	struct file* f = filesys_open (fn_cp);
 
-	  if(f==NULL)
-	  {
-	  	release_filesys_lock();
+	if(f==NULL) {
+		release_filesys_lock();
 	  	return -1;
-	  }
-	  else
-	  {
+	} else {
 	  	file_close(f);
 	  	release_filesys_lock();
 	  	return process_execute(file_name);
-	  }
+	}
 }
 
 void exit(int status)
@@ -227,75 +255,76 @@ void exit(int status)
 	thread_exit();
 }
 
-void* check_valid(const void *vaddr)
+void* validation(const void *vaddr)
 {
-	if (!is_user_vaddr(vaddr))
-	{
+  	if(!is_user_vaddr(vaddr) || vaddr == NULL || vaddr < (void *) 0x08048000 || pagedir_get_page(thread_current()->pagedir, vaddr) == NULL)
 		exit(-1);
-		return 0;
-	}
-	void *file = pagedir_get_page(thread_current()->pagedir, vaddr);
-	if (!file)
-	{
-		exit(-1);
-		return 0;
-	}
-	return file;
+}
+
+void check_buffer (void *buff_to_check, unsigned size)
+{
+  unsigned i;
+  char *ptr  = (char * )buff_to_check;
+  for (i = 0; i < size; i++)
+    {
+      validation((const void *) ptr);
+      ptr++;
+    }
 }
 
 struct proc_file* get_file(struct list* files, int fd)
 {
-
 	struct list_elem *e;
-
-      for (e = list_begin (files); e != list_end (files);
-           e = list_next (e))
-        {
-          struct proc_file *f = list_entry (e, struct proc_file, elem);
-          if(f->fd == fd)
-          	return f;
-        }
-   return NULL;
+    for (e = list_begin (files); e != list_end (files); e = list_next (e)) {
+        struct proc_file *f = list_entry (e, struct proc_file, elem);
+        if(f->fd == fd) return f;
+    }
+   	return NULL;
 }
 
 void close_file(struct list* files, int fd)
 {
 
-	struct list_elem *e;
-
-	struct proc_file *f;
-
-      for (e = list_begin (files); e != list_end (files);
-           e = list_next (e))
-        {
-          f = list_entry (e, struct proc_file, elem);
-          if(f->fd == fd)
-          {
-          	file_close(f->file);
-          	list_remove(e);
-          }
-        }
-
-    free(f);
+	struct list_elem *temp;
+	/* If there are no files in our file_descriptors list, return immediately, */
+	if (!list_empty(&thread_current()->file_descriptors)) {
+	  /* Look to see if the given fd is in our list of file_descriptors. If so, then we
+	     close the file and remove it from our list of file_descriptors. */
+		  for (temp = list_front(files); temp != NULL; temp = temp->next)
+		  {
+		      struct proc_file *t = list_entry (temp, struct proc_file, elem);
+		      if (t->fd == fd)
+		      {
+		        file_close(t->file);
+		        list_remove(&t->elem);
+		        return;
+		      }
+		  }
+	}
 }
 
-void close_all_files(struct list* files)
-{
-
+void close_all_files(struct list* files) {
 	struct list_elem *e;
-
 	while(!list_empty(files))
 	{
 		e = list_pop_front(files);
-
 		struct proc_file *f = list_entry (e, struct proc_file, elem);
-          
 	      	file_close(f->file);
 	      	list_remove(e);
 	      	free(f);
+	}      
+}
 
+void syscall_check_user_string(const char *ustr) {
+  validation(ustr);
 
-	}
-
-      
+  int cnt = 0;
+  while(*ustr != '\0'){
+    if(cnt == 4095) exit(-1);
+    cnt++;
+    ustr++;
+    if (((int)ustr & PGMASK) == 0){
+      validation(ustr);
+    }
+  }
 }
